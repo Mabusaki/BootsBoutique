ecc50a7770f88c6202f908bdbee0f972005331ab
Ferej <ferec125@gmail.com>
2022-11-25 19:42:05 +0400

    first commit

---
+++ src/main/java/com/example/boots/enums/BootType.java
@@ -0,0 +1,20 @@
+package com.example.boots.enums;
+
+public enum BootType {
+    CHELSEA,
+    CHUKKA,
+    DRESS,
+    COWBOY,
+    GOGO,
+    RUSSIAN,
+    UGG,
+    RIGGER,
+    RAIN,
+    RACING,
+    MOTOCROSS,
+    WADER,
+    GALOSH,
+    SNOW,
+    TOURING
+
+}
---
+++ src/main/java/com/example/boots/repositories/BootRepository.java
@@ -0,0 +1,26 @@
+package com.example.boots.repositories;
+
+import com.example.boots.entities.Boot;
+import com.example.boots.enums.BootType;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface BootRepository extends CrudRepository<Boot, Integer> {
+
+    List<Boot> findBootBySize(Float size);
+    List<Boot> findBootByMaterial(String material);
+    List<Boot> findBootByType(BootType type);
+    List<Boot> findByQuantityGreaterThan(Integer minQuantity);
+    List<Boot> findByMaterialAndTypeAndSizeAndQuantityGreaterThan(String material, BootType type, Float size, Integer minQuantity);
+    List<Boot> findByMaterialAndSizeAndType(String material, Float size, BootType type);
+    List<Boot> findByMaterialAndTypeAndQuantityGreaterThan(String material, BootType type, Integer minQuantity);
+    List<Boot> findByMaterialAndType(String material, BootType type);
+    List<Boot> findByTypeAndSizeAndQuantityGreaterThan(BootType type, Float size, Integer minQuantity);
+    List<Boot> findByTypeAndSize(BootType type, Float size);
+    List<Boot> findByTypeAndQuantityGreaterThan(BootType type, Integer minQuantity);
+    List<Boot> findBySizeAndQuantityGreaterThan(Float size, Integer minQuantity);
+
+
+}
